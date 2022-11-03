f67236393ad125a0dfd575e2c8f7d8d1fdd2f1aa
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2022-11-03 09:13:00 +0100

    test input

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $2 --entrypoint=$4 $1 -c "${3//$'\n'/;}"
+echo $1
+# exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $2 --entrypoint=$4 $1 -c "${3//$'\n'/;}"
