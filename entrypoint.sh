9db8c43863d5b3ad0ac99563aca5a97411f07dad
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2022-11-03 13:39:34 +0100

    copy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-echo "Display commands"
-echo "${1//$'\n'/;}"
-# echo "First ls"
-# exec ls
-echo "Execute commands"
-exec ${1//$'\n'/;}
+if [ ! -z $INPUT_USERNAME ];
+then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
+fi
+
+if [ ! -z $INPUT_DOCKER_NETWORK ];
+then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
+fi
+
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
