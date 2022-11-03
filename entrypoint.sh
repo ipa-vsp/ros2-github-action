a70c3d384ce5220f182f38cda409a84f01ba059e
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2022-11-03 14:18:58 +0100

    test 5

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 exec ls
 
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+# exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
