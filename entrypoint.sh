c2bf3d6cfc34f85a62a99c0bf1b62ffe29a45280
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2022-11-04 11:23:01 +0100

    test1

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,6 @@ fi
 ls
 cd colcon_ws
 ls
-cd colcon_ws/src
+cd colcon_ws && cd src
 ls
 exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
